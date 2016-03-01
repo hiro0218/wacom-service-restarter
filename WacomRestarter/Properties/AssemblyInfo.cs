750582d6527b3ddbc8bf9fe1957733e353de4d84
hiro <hiro0218@gmail.com>
2016-03-01 18:58:06 +0900

    メジャーバージョンアップ
    大幅な変更を伴ったのでメジャーバージョンアップした。

--- WacomRestarter/Properties/AssemblyInfo.cs
+++ WacomRestarter/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguage("ja")]
 
